94a54ce6925182d515ba3530b80f4e6b32483e49
sugarmouse <tanghao19950911@gmail.com>
2023-03-24 23:52:47 +0800

    add meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, maximum-scale=1, initial-scale=1, minimum-scale=1, user-scalable=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
