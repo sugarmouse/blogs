73fc5c1ef0805bf25ab0362760cd871f874d7f05
sugarmouse <tanghao19950911@gmail.com>
2023-03-25 15:06:09 +0800

    convert BSR to SSG to display list of blog title

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,21 +1,30 @@
-import { NextPage } from 'next';
-import { usePosts } from '@/hooks/usePosts';
+import { GetStaticProps, NextPage } from 'next';
+import getPosts from '@/lib/posts/posts';
 
-const PostsIndex: NextPage = () => {
-  const { isLoading, isEmpty, posts } = usePosts();
+type Props = {
+  posts: Post[];
+};
 
+const PostsIndex: NextPage<Props> = (props) => {
+  const { posts } = props;
   return (
     <>
       <h1>Pages Index</h1>
-      {isLoading ? (
-        <h2>加载中 </h2>
-      ) : isEmpty ? (
-        <div>列表为空</div>
-      ) : (
-        posts.map((post) => <div key={post.id}>{post.title}</div>)
-      )}
+      {posts.map((p) => (
+        <div key={p.id}>{p.title}</div>
+      ))}
     </>
   );
 };
 
 export default PostsIndex;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+  };
+};
