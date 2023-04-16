a6f7497701b589fba93eb5652389a9401b204a8f
sugarmouse <tanghao19950911@gmail.com>
2023-04-16 23:57:44 +0800

    refctor new page

--- pages/posts/new.tsx
+++ pages/posts/new.tsx
@@ -1,70 +1,54 @@
 import { GetServerSideProps } from 'next';
-import { Form } from '@/components/Form';
 import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import { useForm } from '@/hooks/useForm';
+import axios from 'axios';
+import type { Field } from '@/hooks/useForm';
 
 type ThisFormData = {
   title: string;
   content: string;
 };
 
-type ThisErrors = {
-  title: string[];
-  content: string[];
-};
-
 const NewPost = () => {
-  const [formData, setFromData] = useState<ThisFormData>({
+  const initFormData: ThisFormData = {
     title: '',
     content: '',
-  });
+  };
 
-  const [errors, setErrors] = useState<ThisErrors>({
-    title: [],
-    content: [],
-  });
-
-  const onChange = useCallback(
-    (key: keyof ThisFormData, value: string | number) => {
-      setFromData({
-        ...formData,
-        [key]: value,
-      });
+  const fields: Field[] = [
+    {
+      label: '标题',
+      type: 'text',
+      key: 'title',
     },
-    [formData]
-  );
-
-  const onSubmit = useCallback(
-    (e: FormEvent) => {
-      e.preventDefault();
-      console.log(formData);
+    {
+      label: '内容',
+      type: 'textarea',
+      key: 'content',
     },
-    [formData]
+  ];
+
+  const onSubmit = (formData: ThisFormData) => {
+    axios.post('/posts/new', formData).then(
+      () => {
+        window.alert('提交成功');
+      },
+      (e) => {
+        setErrors(e);
+      }
+    );
+  };
+
+  const button = <button type="submit">注册</button>;
+
+  const { form, setErrors } = useForm<ThisFormData>(
+    initFormData,
+    fields,
+    onSubmit,
+    button
   );
 
-  return (
-    <>
-      <Form
-        fields={[
-          {
-            label: '标题',
-            type: 'text',
-            value: formData.title,
-            onChange: (e) => onChange('title', e.target.value),
-            errors: errors.title,
-          },
-          {
-            label: '内容',
-            type: 'textarea',
-            value: formData.content,
-            onChange: (e) => onChange('content', e.target.value),
-            errors: errors.content,
-          },
-        ]}
-        onSubmit={onSubmit}
-        button={<button type="submit">创建</button>}
-      />
-    </>
-  );
+  return <>{form}</>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
